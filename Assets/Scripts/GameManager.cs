4f104ea9f41061d6951213a866cf348190c076c4
RavenTheNecromancer <dimitar.boyadzhiev02@gmail.com>
2023-08-27 00:25:07 +0300

    Added Essential Folders

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Array initialisation
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Actuall logic
+    }
+}
